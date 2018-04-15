0031f2b94765793265e0dbe370d89585a55e22fc
Nihal Reddy Gurrala <Nihalreddygurrala@nihals-mbp>
2018-04-14 20:10:43 -0400

    first commit

---
+++ src/main/java/com/nihal/springbootweb/practiceBootWeb/PracticeBootWebApplication.java
@@ -0,0 +1,12 @@
+package com.nihal.springbootweb.practiceBootWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeBootWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticeBootWebApplication.class, args);
+	}
+}
