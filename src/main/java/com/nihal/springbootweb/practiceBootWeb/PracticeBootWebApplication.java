2cb9d187b958700b2ffceac71b378a386122f9f1
Nihal Reddy Gurrala <Nihalreddygurrala@nihals-mbp>
2018-04-15 20:16:29 -0400

    Changes in Pom.xml

--- src/main/java/com/nihal/springbootweb/practiceBootWeb/PracticeBootWebApplication.java
+++ src/main/java/com/nihal/springbootweb/practiceBootWeb/PracticeBootWebApplication.java
@@ -1,9 +1,11 @@
 package com.nihal.springbootweb.practiceBootWeb;
 
+import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Configurable
 public class PracticeBootWebApplication {
 
 	public static void main(String[] args) {
